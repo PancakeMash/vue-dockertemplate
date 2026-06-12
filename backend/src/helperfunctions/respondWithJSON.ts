17cf0282ffd4940a19534767dfeb136989154fbb
PancakeMash <arpandevofficial@gmail.com>
2026-06-12 15:05:57 +0100

    First stages of docker template

--- backend/src/helperfunctions/respondWithJSON.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export function respondWithJSON(res: Response, code: number, payload: any) {
-    res.header("Content-Type", "application/json");
-    res.status(code).json(payload);
-}
