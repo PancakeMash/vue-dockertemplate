45ab3c8097290140a6a442284403c352e159fc2f
PancakeMash <arpandevofficial@gmail.com>
2026-06-13 16:03:40 +0100

    Added sass as styling of choice to finish setting up template

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,6 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { config } from "./config.js";
 
-// await runMigrations();
-
 const env = process.env;
 const PORT = env.API_PORT || 3000;
 const API_URL = env.API_URL;
@@ -32,6 +30,32 @@ app.get("/api/health", (req: Request, res: Response) => {
 
 app.use(middlewareErrorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at ${API_URL}:${PORT}`);
-});
+
+startServer();
+
+async function startServer() {
+  await runMigrations();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at ${API_URL}:${PORT}`);
+  });
+}
+
+async function runMigrations() {
+  try {
+    const migrationClient = postgres({
+      host: config.db.host,
+      port: config.db.port,
+      username: config.db.user,
+      password: config.db.password,
+      database: config.db.database,
+      max: 1,
+    });
+    await migrate(drizzle(migrationClient), config.db.migrationConfig);
+    console.log("✅ Migrations completed!");
+  } catch (error) {
+    console.error("❌ Migration failed, retrying in 5 seconds...", error);
+    await new Promise(res => setTimeout(res, 5000));
+    return runMigrations(); // Recursive retry
+  }
+}
